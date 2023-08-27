0cd9e9b47e44b98fe9a798dc3977637498f15e9c
davischristianson <dav.christian10@gmail.com>
2023-08-26 18:45:47 -0700

    Made a Create Paint view

---
+++ CPW219-eCommerceSite/Controllers/PaintController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class PaintController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

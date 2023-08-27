86870bd25c303237ab7efe64127df99587303718
davischristianson <dav.christian10@gmail.com>
2023-08-26 19:05:39 -0700

    Create paint is fully functional, added some styling

--- CPW219-eCommerceSite/Controllers/PaintController.cs
+++ CPW219-eCommerceSite/Controllers/PaintController.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using CPW219_eCommerceSite.Models;
+using CPW219_eCommerceSite.Data;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class PaintController : Controller
     {
+        private readonly PaintProductContext _context;
+
+        public PaintController(PaintProductContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Paint p)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Paints.Add(p); // Prepares insert
+                _context.SaveChanges(); // Executes pending insert
+
+                ViewData["Message"] = $"{p.Title} was added successfully!";
+                return View();
+            }
+
+            return View(p);
         }
     }
 }
